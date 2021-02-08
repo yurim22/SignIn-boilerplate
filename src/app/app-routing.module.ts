033bc9a2f608e6222a0027c4d70351dc71a240aa
yurim <yrseo1216@gmail.com>
2021-02-08 11:24:54 +0900

    start project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'signin',
+        pathMatch: 'full'
+    },
+    // {
+    //     path: 'signin',
+    //     component: SignInComponent
+    // }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {useHash: true})],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
